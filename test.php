933cf63f70d79da18f40d21bd6a251353ab0d376
dbmuthumari <41888181+dbmuthumari@users.noreply.github.com>
2018-07-30 08:37:43 +0000

    Add files via upload

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function processMessage($update) {
+    if($update["result"]["action"] == "sayHello"){
+        sendMessage(array(
+            "source" => $update["result"]["source"],
+            "speech" => "Hello from webhook",
+            "displayText" => "Hello from webhook",
+            "contextOut" => array()
+        ));
+    }
+}
+
+function sendMessage($parameters) {
+    echo json_encode($parameters);
+}
+
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+if (isset($update["result"]["action"])) {
+    processMessage($update);
+}
